f6f04c4e80979c81e55317a0f0104a05b46fbf51
Angga Fantiya Hermawan <anggafantiya@gmail.com>
2020-05-12 10:36:31 +0700

    Membuat Tampilan Panggilan Darurat

--- app/src/main/java/com/example/tubesmoprog/HalamanUtamaActivity.java
+++ app/src/main/java/com/example/tubesmoprog/HalamanUtamaActivity.java
@@ -2,13 +2,30 @@ package com.example.tubesmoprog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HalamanUtamaActivity extends AppCompatActivity {
-
+Button btnPDarurat;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_halaman_utama);
+        Button btnPDarurat=(Button) findViewById(R.id.btn_pDarurat);
+
+        // Start = fungsi ketika klik button panggilan darurat akan langsung pindah ke kontak telp
+        btnPDarurat.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                String nomor = "119";
+                Intent telp = new Intent(Intent.ACTION_VIEW);
+                telp.setData(Uri.fromParts("tel",nomor,null));
+                startActivity(telp);
+            }
+        });
+        //End = fungsi ketika klik button panggilan darurat akan langsung pindah ke kontak telp
     }
 }
