29b3a1b6d878d0a6e843b68a00669058b79bdad7
Angga Fantiya Hermawan <anggafantiya@gmail.com>
2020-05-17 11:41:21 +0700

    Nambahin fungsi intent di HalamanAwal

--- app/src/main/java/com/example/tubesmoprog/HalamanAwalActivity.java
+++ app/src/main/java/com/example/tubesmoprog/HalamanAwalActivity.java
@@ -2,13 +2,37 @@ package com.example.tubesmoprog;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class HalamanAwalActivity extends AppCompatActivity {
-
+public class HalamanAwalActivity extends AppCompatActivity implements View.OnClickListener {
+Button btnRegistt,btnLoginn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_halaman_awal);
+
+        btnRegistt=(Button) findViewById(R.id.btn_registt);
+        btnLoginn=(Button) findViewById(R.id.btn_loginn);
+
+        //eksekusi button
+        btnRegistt.setOnClickListener(this);
+        btnLoginn.setOnClickListener(this);
     }
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.btn_loginn:
+                Intent  moveToPageLogin =new Intent(HalamanAwalActivity.this,LoginActivity.class);
+                startActivity(moveToPageLogin);
+                break;
+            case R.id.btn_registt:
+                Intent moveToPageRegis = new Intent(HalamanAwalActivity.this,RegisterActivity.class);
+                startActivity(moveToPageRegis);
+                break;
+        }
+    }
+
+
 }
