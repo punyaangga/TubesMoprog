eeb488cf68df7527f39d24c8e7c3f7cdad15983e
Angga Fantiya Hermawan <anggafantiya@gmail.com>
2020-05-17 17:20:07 +0700

    Membuat backedn menampilkan list daftar rs

--- app/src/main/java/com/example/tubesmoprog/DaftarRumahSakit.java
+++ app/src/main/java/com/example/tubesmoprog/DaftarRumahSakit.java
@@ -1,14 +1,59 @@
 package com.example.tubesmoprog;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class DaftarRumahSakit extends AppCompatActivity {
+    String[] daftar;
+    ListView ListView01;
+    Menu menu;
+    protected Cursor cursor;
+    DataHelper dbcenter;
+    public static DaftarRumahSakit da;
+    Button btnBack;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daftar_rumah_sakit);
+//        btnBack=(Button) findViewById(R.id.btnBack);
+//        btnBack.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View arg0) {
+//                Intent inte = new Intent(DaftarRumahSakit.this, AdminRsRujukan.class);
+//                startActivity(inte);
+//            }
+//        });
+        da = this; //nambahin ini
+        dbcenter = new DataHelper(this);
+        RefreshList();
+    }
+
+    public void RefreshList() {
+        SQLiteDatabase db = dbcenter.getReadableDatabase();
+        cursor = db.rawQuery("SELECT * FROM rs", null);
+        daftar = new String[cursor.getCount()];
+        cursor.moveToFirst();
+        for (int cc = 0; cc < cursor.getCount(); cc++) {
+            cursor.moveToPosition(cc);
+            daftar[cc] = cursor.getString(1).toString() + "-" + cursor.getString(2).toString();
+        }
+        ListView01 = (ListView) findViewById(R.id.listView1);
+        ListView01.setAdapter(new ArrayAdapter(this, android.R.layout.simple_list_item_1, daftar));
+
     }
 }
+
+
