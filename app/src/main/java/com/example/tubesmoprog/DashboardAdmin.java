9e4b6a1d16e9df004df5f6aa199b864f8bbaa137
Angga Fantiya Hermawan <anggafantiya@gmail.com>
2020-06-01 15:52:42 +0700

    dashboard admin

---
+++ app/src/main/java/com/example/tubesmoprog/DashboardAdmin.java
@@ -0,0 +1,27 @@
+package com.example.tubesmoprog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class DashboardAdmin extends AppCompatActivity {
+    Button btnAdminRsRujukan,btnAdminKonten;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard_admin);
+        btnAdminRsRujukan=(Button)findViewById(R.id.btn_AdminRsRujukan);
+        btnAdminKonten = (Button) findViewById(R.id.btn_AdminEdukasi);
+
+        btnAdminRsRujukan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iListRs = new Intent(getApplicationContext(),AdminListRsRujukan.class);
+                startActivity(iListRs);
+            }
+        });
+    }
+}
