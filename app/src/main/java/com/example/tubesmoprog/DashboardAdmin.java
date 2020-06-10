25873357acfbaeb385ebba0a094c2cc30cfbb2f0
Angga Fantiya Hermawan <anggafantiya@gmail.com>
2020-06-10 14:19:43 +0700

    Menu Biodata Anggota

--- app/src/main/java/com/example/tubesmoprog/DashboardAdmin.java
+++ app/src/main/java/com/example/tubesmoprog/DashboardAdmin.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class DashboardAdmin extends AppCompatActivity {
-    Button btnAdminRsRujukan,btnAdminKonten,btnLogout;
+    Button btnAdminRsRujukan,btnAdminKonten,btnLogout,btnProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +17,16 @@ public class DashboardAdmin extends AppCompatActivity {
         btnAdminRsRujukan=(Button)findViewById(R.id.btn_AdminRsRujukan);
         btnAdminKonten = (Button) findViewById(R.id.btn_AdminEdukasi);
         btnLogout = (Button) findViewById(R.id.btn_Logout);
+        btnProfile = (Button) findViewById(R.id.btnProfile);
+
+        btnProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent iProfilee = new Intent(getApplicationContext(),infoProfile.class);
+                startActivity(iProfilee);
+            }
+        });
+
 
         btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,8 +56,16 @@ public class DashboardAdmin extends AppCompatActivity {
         startActivity(iListKonten);
     }
     public  void PindahRs(View v){
-        Intent iListKonten = new Intent(getApplicationContext(),AdminListKontenEdukasi.class);
+        Intent iListKonten = new Intent(getApplicationContext(),AdminListRsRujukan.class);
         startActivity(iListKonten);
+    }
+    public void Logout(View v){
+        Intent iLogoutApp = new Intent(getApplicationContext(),LoginActivity.class);
+        startActivity(iLogoutApp);
+    }
+    public void Profile(View v){
+        Intent iProfile = new Intent(getApplicationContext(),infoProfile.class);
+        startActivity(iProfile);
     }
 
 }
